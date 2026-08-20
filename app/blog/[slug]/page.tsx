74876f0c84ef0f9c02bff5601cd985d71870e322
Cici Woolf <christinemarie.woolf@gmail.com>
2026-08-20 13:33:17 -0500

    fix(blog): remove unused params to fix TypeScript warning

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,10 +1,7 @@
 import { notFound } from 'next/navigation'
 
-export default function BlogPostPage({
-  params,
-}: {
-  params: { slug: string }
-}) {
+export default function BlogPostPage() {
   // For now, return 404 for all blog posts
+  // params will be used when blog posts are implemented
   notFound()
 }
